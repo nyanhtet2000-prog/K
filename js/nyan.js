2a23dfafd63bfb80b18a6040546406698ab721cb
nyanhtet2000-prog <nyanhtet2000@gmail.com>
2026-01-14 12:01:16 +0900

    Add files via upload

---
+++ js/nyan.js
@@ -0,0 +1,8 @@
+const humburger = document.getElementById("humburger");
+const navMenu = document.getElementById("navMenu");
+ 
+humburger.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+});
+ 
+ 
